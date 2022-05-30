f048c1a558acd1ee6d8867287a691adc1a7b4fc1
yangwenkaipaul <paul>
2022-05-30 14:27:14 +0800

    提交修改

--- src/android/GlobalCoordinatesUtils.java
+++ src/android/GlobalCoordinatesUtils.java
@@ -3,7 +3,6 @@ package cordova.plugin.geodetic;
 import org.gavaghan.geodesy.Ellipsoid;
 import org.gavaghan.geodesy.GeodeticCalculator;
 import org.gavaghan.geodesy.GlobalCoordinates;
-import org.springframework.util.Assert;
 
 /**
  * User: FunkyZc
@@ -20,10 +19,6 @@ public final class GlobalCoordinatesUtils {
 	}
 
 	public static double getDistance(Double longitudeFrom, Double latitudeFrom, Double longitudeTo, Double latitudeTo) {
-		Assert.notNull(longitudeFrom, "传入经度不能为空");
-		Assert.notNull(latitudeFrom, "传入纬度不能为空");
-		Assert.notNull(longitudeTo, "接收经度不能为空");
-		Assert.notNull(longitudeFrom, "接收纬度不能为空");
 		GlobalCoordinates source = new GlobalCoordinates(latitudeFrom, longitudeFrom);
 		GlobalCoordinates target = new GlobalCoordinates(latitudeTo, longitudeTo);
 
